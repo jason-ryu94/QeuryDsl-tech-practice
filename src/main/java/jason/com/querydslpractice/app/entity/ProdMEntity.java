79d1ea166dead17cbcbbd96342610243f9be6a77
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-05 19:11:11 +0900

    entity 생성

--- src/main/java/jason/com/querydslpractice/app/entity/ProdMEntity.java
+++ src/main/java/jason/com/querydslpractice/app/entity/ProdMEntity.java
@@ -1,4 +1,16 @@
 package jason.com.querydslpractice.app.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ProdMEntity {
+
+    @Id
+    private String prodMId;
+
+    private String prodCd;
+
+    private String prodNm;
+
 }
