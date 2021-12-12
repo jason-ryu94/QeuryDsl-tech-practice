126106b3c28b67f575370f021ad9d6448467c392
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-12 21:29:46 +0900

    service create

---
+++ src/main/java/jason/com/querydslpractice/app/service/QueryDslPractice.java
@@ -0,0 +1,11 @@
+package jason.com.querydslpractice.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryDslPractice {
+
+
+}
