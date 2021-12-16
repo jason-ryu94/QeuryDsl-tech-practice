c78585170ca00229256f9b363d2e24382b62bbaf
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-16 23:57:15 +0900

    service update

--- src/main/java/jason/com/querydslpractice/app/service/QueryDslPractice.java
+++ src/main/java/jason/com/querydslpractice/app/service/QueryDslPractice.java
@@ -1,10 +1,12 @@
 package jason.com.querydslpractice.app.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QueryDslPractice {
 
 
