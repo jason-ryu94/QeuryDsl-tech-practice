31849d166d52a2d1e85d73c249be77d7a5b2f0ed
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-17 22:42:30 +0900

    repository

--- src/main/java/jason/com/querydslpractice/app/repository/ProdReposiroty.java
+++ src/main/java/jason/com/querydslpractice/app/repository/ProdReposiroty.java
@@ -1,7 +1,9 @@
 package jason.com.querydslpractice.app.repository;
 
+import jason.com.querydslpractice.app.entity.ProdEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdReposiroty {
+public interface ProdReposiroty extends JpaRepository<ProdEntity, String> {
 }
