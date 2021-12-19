3a3c4c683a893b2c621785f49fb435450fcc7158
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-20 02:44:32 +0900

    prodMRepository update

--- src/main/java/jason/com/querydslpractice/app/repository/ProdMRepository.java
+++ src/main/java/jason/com/querydslpractice/app/repository/ProdMRepository.java
@@ -1,4 +1,7 @@
 package jason.com.querydslpractice.app.repository;
 
-public interface ProdMRepository {
+import jason.com.querydslpractice.app.entity.ProdMEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdMRepository extends JpaRepository<ProdMEntity, String> {
 }
