ccfe565511a6ffbacabb5c7c1bfd39cce9fbc654
jason-ryu94 <wptmd0918@hanmail.net>
2021-12-24 23:47:09 +0900

    generated Value

--- src/main/java/jason/com/querydslpractice/app/entity/ProdMEntity.java
+++ src/main/java/jason/com/querydslpractice/app/entity/ProdMEntity.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -15,6 +16,7 @@ import javax.persistence.Id;
 public class ProdMEntity {
 
     @Id
+    @GeneratedValue
     private String prodMId;
 
     private String prodCd;
